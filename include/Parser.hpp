56b40d63c47754d07dbc7f3ac7f0a435b836489b
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-13 20:33:48 -0200

    Refactor Parser

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,5 +1,5 @@
-#ifndef HASM_PARSER_H_
-#define HASM_PARSER_H_
+#ifndef HASM_PARSER_HPP
+#define HASM_PARSER_HPP
 
 #include <iostream>
 #include <string>
@@ -7,54 +7,56 @@
 #include "Hasm.hpp"
 
 namespace Hasm {
-  class Parser {
-    public:
-      enum class Status {
+
+class Parser {
+  public:
+    enum class Status {
         START_OF_FILE,
         VALID_COMMAND,
         INVALID_COMMAND,
         END_OF_FILE
-      };
+    };
 
-      explicit Parser(std::istream& input);
+    explicit Parser(std::istream& input);
 
-      bool advance();
+    bool advance();
 
-      Status getStatus() const;
-      const std::string& getCommand() const;
-      CommandType getCommandType() const;
-      int getCurrentLineNumber() const;
+    Status getStatus() const;
+    const std::string& getCommand() const;
+    CommandType getCommandType() const;
+    int getCurrentLineNumber() const;
 
-      std::string symbol() const;
-      std::string dest() const;
-      std::string comp() const;
-      std::string jump() const;
+    std::string symbol() const;
+    std::string dest() const;
+    std::string comp() const;
+    std::string jump() const;
 
-      bool reset();
+    bool reset();
 
-    private:
-      bool readNextLine(std::string& str);
+  private:
+    bool readNextLine(std::string& str);
 
-      void update(const std::string& newCommand);
-      void setCommand(const std::string& newCommand);
-      void updateStatus();
-      void checkErrors();
+    void update(const std::string& newCommand);
+    void setCommand(const std::string& newCommand);
+    void updateStatus();
+    void checkErrors();
 
-      void trim(std::string& str) const;
-      void removeComments(std::string& str) const;
-      void removeSpaces(std::string& str) const;
+    void trim(std::string& str) const;
+    void removeComments(std::string& str) const;
+    void removeSpaces(std::string& str) const;
 
-      bool isValidCommand() const;
-      bool isACommand() const;
-      bool isCCommand() const;
-      bool isLCommand() const;
+    bool isValidCommand() const;
+    bool isACommand() const;
+    bool isCCommand() const;
+    bool isLCommand() const;
 
-      std::istream& input;
-      std::string command = "";
-      mutable CommandType commandType = CommandType::INVALID;
-      int lineNumber = 0;
-      Status status = Status::START_OF_FILE;
-  };
-}
+    std::istream& input;
+    std::string command{""};
+    mutable CommandType commandType{CommandType::INVALID};
+    int lineNumber{0};
+    Status status{Status::START_OF_FILE};
+};
+
+} // namespace Hasm
 
 #endif
