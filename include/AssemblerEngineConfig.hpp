2c248285091e5568c47f11be9a1e0ff45a15e6b3
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-13 21:24:46 -0200

    Refactor AssemblerEngineConfig

--- include/AssemblerEngineConfig.hpp
+++ include/AssemblerEngineConfig.hpp
@@ -1,18 +1,20 @@
-#ifndef HASM_ASSEMBLERENGINECONFIG_H_
-#define HASM_ASSEMBLERENGINECONFIG_H_
+#ifndef HASM_ASSEMBLERENGINECONFIG_HPP
+#define HASM_ASSEMBLERENGINECONFIG_HPP
 
 #include <string>
 
 namespace Hasm {
-  class AssemblerEngineConfig {
-    public:
-      AssemblerEngineConfig(bool isValid, bool exportSymbols, std::string inputName)
-          : isValid{isValid}, exportSymbols{exportSymbols}, inputName{inputName} { }
 
-      const bool isValid;
-      const bool exportSymbols;
-      const std::string inputName;
-  };
-}
+class AssemblerEngineConfig {
+  public:
+    AssemblerEngineConfig(const bool isValid, const bool exportSymbols, std::string inputName)
+        : isValid{isValid}, exportSymbols{exportSymbols}, inputName{inputName} {}
+
+    const bool isValid;
+    const bool exportSymbols;
+    const std::string inputName;
+};
+
+} // namespace Hasm
 
 #endif
