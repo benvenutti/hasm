77e022825b121cfac2a24662efbd5c302f47a70b
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-13 19:30:40 -0200

    Refactor ErrorMessage

--- include/ErrorMessage.h
+++ include/ErrorMessage.h
@@ -4,10 +4,12 @@
 #include <string>
 
 namespace Hasm {
-  namespace ErrorMessage {
-    std::string invalidCommand(const std::string &cmd, const int lineNumber);
-    std::string invalidLoadValue(const std::string &cmd, const int lineNumber);
-  };
-};
+namespace ErrorMessage {
+
+std::string invalidCommand(const std::string& cmd, const int lineNumber);
+std::string invalidLoadValue(const std::string& cmd, const int lineNumber);
+
+} // namespace ErrorMessage
+} // namespace Hasm
 
 #endif
--- src/ErrorMessage.cpp
+++ src/ErrorMessage.cpp
@@ -2,19 +2,25 @@
 
 #include <sstream>
 
-namespace {
-  std::string message(const std::string& cmd, const int lineNumber, const std::string& info) {
-    std::stringstream ss;
-    ss << "line " << lineNumber << ": error: \"" << cmd << "\" " << info;
+namespace Hasm {
 
-    return ss.str();
-  }
+namespace {
+
+std::string message(const std::string& cmd, const int lineNumber, const std::string& info) {
+  std::stringstream ss{};
+  ss << "line " << lineNumber << ": error: \"" << cmd << "\" " << info;
+
+  return ss.str();
 }
 
-std::string Hasm::ErrorMessage::invalidCommand(const std::string &cmd, const int lineNumber) {
+} // unnamed namespace
+
+std::string ErrorMessage::invalidCommand(const std::string& cmd, const int lineNumber) {
   return message(cmd, lineNumber, "is an invalid command");
 }
 
-std::string Hasm::ErrorMessage::invalidLoadValue(const std::string &cmd, const int lineNumber) {
+std::string ErrorMessage::invalidLoadValue(const std::string& cmd, const int lineNumber) {
   return message(cmd, lineNumber, "loads a value greater than an unsigned 15-bit number");
 }
+
+} // namespace Hasm
