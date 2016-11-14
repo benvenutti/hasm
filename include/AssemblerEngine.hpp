49c3928460748439a51c84b4058ffbd7efe7e32f
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-14 21:48:27 -0200

    Refactor AssemblerEngine

--- include/AssemblerEngine.hpp
+++ include/AssemblerEngine.hpp
@@ -1,22 +1,24 @@
-#ifndef HASM_ASSEMBLERENGINE_H_
-#define HASM_ASSEMBLERENGINE_H_
+#ifndef HASM_ASSEMBLERENGINE_HPP
+#define HASM_ASSEMBLERENGINE_HPP
 
-#include <iostream>
+#include <ostream>
 #include <string>
 
 namespace Hasm {
-  class AssemblerEngineConfig;
-  class SymbolTable;
 
-  class AssemblerEngine {
-    public:
-      bool run(int argc, char** argv) const;
+class AssemblerEngineConfig;
+class SymbolTable;
 
-    private:
-      bool isAsmFile(const std::string& fileName) const;
-      bool exportSymbolTable(const AssemblerEngineConfig& cfg, const SymbolTable& table) const;
-      void outputSymbolTable(std::ostream& out, const SymbolTable& table) const;
-  };
-}
+class AssemblerEngine {
+  public:
+    bool run(int argc, char** argv) const;
+
+  private:
+    bool isAsmFile(const std::string& fileName) const;
+    bool exportSymbolTable(const AssemblerEngineConfig& cfg, const SymbolTable& table) const;
+    void outputSymbolTable(std::ostream& out, const SymbolTable& table) const;
+};
+
+} // namespace Hasm
 
 #endif
