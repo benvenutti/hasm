196cab74190991a68cb08f9fcd7011c7efa4b322
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2016-11-14 21:59:57 -0200

    Refactor Assembler

--- include/Assembler.hpp
+++ include/Assembler.hpp
@@ -1,35 +1,38 @@
-#ifndef HASM_ASSEMBLER_H_
-#define HASM_ASSEMBLER_H_
+#ifndef HASM_ASSEMBLER_HPP
+#define HASM_ASSEMBLER_HPP
 
 #include <iostream>
 
 #include "Hack.hpp"
+#include "Hasm.hpp"
 #include "Parser.hpp"
 #include "SymbolTable.hpp"
 
 namespace Hasm {
-  class Assembler {
-    public:
-      Assembler(std::istream& in, std::ostream& out);
 
-      bool assemble();
-      const SymbolTable& getSymbolTable() const;
+class Assembler {
+  public:
+    Assembler(std::istream& in, std::ostream& out);
 
-    private:
-      bool firstPass();
-      bool secondPass();
-      bool assembleCommand(const Hasm::CommandType commandType);
-      bool assembleACommand();
-      bool assembleCCommand();
-      Hack::WORD computeValue(const std::string& symbol);
-      bool isValidValue(const Hack::WORD value) const;
-      void output(const Hack::WORD word);
+    bool assemble();
+    const SymbolTable& getSymbolTable() const;
 
-      std::ostream& out;
-      Parser parser;
-      SymbolTable symbolTable{Hack::PREDEFINED_SYMBOLS};
-      Hack::WORD RAMaddress{Hack::INIT_RAM_ADDRESS};
-  };
-}
+  private:
+    bool firstPass();
+    bool secondPass();
+    bool assembleCommand(const CommandType commandType);
+    bool assembleACommand();
+    bool assembleCCommand();
+    Hack::WORD computeValue(const std::string& symbol);
+    bool isValidValue(const Hack::WORD value) const;
+    void output(const Hack::WORD word);
+
+    std::ostream& out;
+    Parser parser;
+    SymbolTable symbolTable{Hack::PREDEFINED_SYMBOLS};
+    Hack::WORD RAMaddress{Hack::INIT_RAM_ADDRESS};
+};
+
+} // namespace Hasm
 
 #endif
