37d26d6f20a44d29644428ce6ba2754de5c42710
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2017-07-19 20:54:39 -0300

    Fix CMake update

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -3,12 +3,6 @@
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo -E apt-get update -qq
   sudo -E apt-get install -y libboost-all-dev
-
-  mkdir $HOME/usr
-  export PATH="$HOME/usr/bin:$PATH"
-  wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh
-  chmod +x cmake-3.8.2-Linux-x86_64.sh
-  ./cmake-3.8.2-Linux-x86_64.sh --prefix=$HOME/usr --exclude-subdir --skip-license
 fi
 
 if [ "$IS_COVERAGE_BUILD" == 1 ]; then
