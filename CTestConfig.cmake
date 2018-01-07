f1f93ff11f9349b7a9216385044c783735872c02
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-01-07 20:19:23 +0100

    Remove comments from config file

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,11 +1,3 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-##
-## # The following are required to submit to the CDash dashboard:
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
-
 set(CTEST_PROJECT_NAME "hasm")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
