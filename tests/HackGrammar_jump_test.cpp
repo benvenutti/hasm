95d39312705fba0af6beb2325a0cf1a7f471f022
Diogo Benvenutti <diogo.benvenutti@native-instruments.de>
2018-07-19 23:11:47 +0200

    Fix clash of test names

--- tests/HackGrammar_jump_test.cpp
+++ tests/HackGrammar_jump_test.cpp
@@ -9,39 +9,39 @@ struct FixtureJumps {
 
 BOOST_FIXTURE_TEST_SUITE(jumps, FixtureJumps)
 
-BOOST_AUTO_TEST_CASE(test_validJGT) {
+BOOST_AUTO_TEST_CASE(jump_test_validJGT) {
   BOOST_CHECK(boost::regex_match("JGT", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJGE) {
+BOOST_AUTO_TEST_CASE(jump_test_validJGE) {
   BOOST_CHECK(boost::regex_match("JGE", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJLT) {
+BOOST_AUTO_TEST_CASE(jump_test_validJLT) {
   BOOST_CHECK(boost::regex_match("JLT", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJLE) {
+BOOST_AUTO_TEST_CASE(jump_test_validJLE) {
   BOOST_CHECK(boost::regex_match("JLE", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJEQ) {
+BOOST_AUTO_TEST_CASE(jump_test_validJEQ) {
   BOOST_CHECK(boost::regex_match("JEQ", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJNE) {
+BOOST_AUTO_TEST_CASE(jump_test_validJNE) {
   BOOST_CHECK(boost::regex_match("JNE", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_validJMP) {
+BOOST_AUTO_TEST_CASE(jump_test_validJMP) {
   BOOST_CHECK(boost::regex_match("JMP", jump));
 }
 
-BOOST_AUTO_TEST_CASE(test_invalidEmptyJump) {
+BOOST_AUTO_TEST_CASE(jump_test_invalidEmptyJump) {
   BOOST_CHECK_EQUAL(boost::regex_match("", jump), false);
 }
 
-BOOST_AUTO_TEST_CASE(test_invalidJump) {
+BOOST_AUTO_TEST_CASE(jump_test_invalidJump) {
   BOOST_CHECK_EQUAL(boost::regex_match("JP", jump), false);
 }
 
