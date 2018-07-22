443a5e0535c61d1d6876f3528902e8800704cad4
Diogo Benvenutti <diogo.benvenutti@native-instruments.de>
2018-07-23 00:17:01 +0200

    Add final tests for HackCommandParser

--- tests/HackCommandParser.computation.test.cpp
+++ tests/HackCommandParser.computation.test.cpp
@@ -1,29 +1,74 @@
 #include "HackCommandParser.hpp"
 
+#include <boost/range/join.hpp>
 #include <catch2/catch.hpp>
 
-SCENARIO( "parse labels", "[HackCommandParser]" )
+#include <string>
+#include <vector>
+
+namespace detail
 {
-    REQUIRE( Hasm::HackCommandParser::isLabelCommand( "(_id)" ) );
-    REQUIRE_FALSE( Hasm::HackCommandParser::isLabelCommand( "(_id" ) );
+
+const std::vector<std::string> dest{ "M", "D", "MD", "A", "AM", "AD", "AMD" };
+
+const std::vector<std::string> comp{ "0",   "1",   "-1",  "D",   "A",   "!D",  "!A",  "-D",  "-A", "D+1",
+                                     "A+1", "D-1", "A-1", "D+A", "D-A", "A-D", "D&A", "D|A", "M",  "!M",
+                                     "-M",  "M+1", "M-1", "D+M", "D-M", "M-D", "D&M", "D|M" };
+
+const std::vector<std::string> jump{ "JGT", "JEQ", "JGE", "JLT", "JNE", "JLE", "JMP" };
+
+std::vector<std::string> combine( const std::vector<std::string>& itens1,
+                                  const std::vector<std::string>& itens2,
+                                  const std::string&              separator )
+{
+    std::vector<std::string> result;
+    result.reserve( itens1.size() * itens2.size() );
+
+    for ( const auto& a : itens1 )
+    {
+        for ( const auto& b : itens2 )
+        {
+            result.push_back( a + separator + b );
+        }
+    }
+
+    return result;
 }
 
-SCENARIO( "parse computations", "[HackCommandParser]" )
+std::vector<std::string> comps()
 {
-    REQUIRE( Hasm::HackCommandParser::isComputationCommand( "D+1" ) );
+    const auto destAndComp        = combine( dest, comp, "=" );
+    const auto compAndJump        = combine( comp, jump, ";" );
+    const auto destAndCompAndJump = combine( destAndComp, jump, ";" );
+
+    return boost::copy_range<std::vector<std::string>>(
+        boost::join( boost::join( boost::join( comp, destAndComp ), compAndJump ), destAndCompAndJump ) );
+}
+
+const auto allComps = comps();
+
+} // namespace detail
+
+SCENARIO( "parse valid computations", "[HackCommandParser]" )
+{
+    for ( const auto& comp : detail::allComps )
+    {
+        REQUIRE( Hasm::HackCommandParser::isComputationCommand( comp ) );
+    }
+}
+
+SCENARIO( "parse invalid computations", "[HackCommandParser]" )
+{
+    // invalid computations
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D+X" ) );
 
-    // destinations
-    REQUIRE( Hasm::HackCommandParser::isComputationCommand( "D=D+A" ) );
+    // invalid destinations
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "X=D+A" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "1=D+A" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "=D+A" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=X" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=2" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=" ) );
-
-    // valid computation and jump
-    REQUIRE( Hasm::HackCommandParser::isComputationCommand( "D+A;JMP" ) );
 
     // invalid computation and valid jump
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "X;JMP" ) );
@@ -34,9 +79,6 @@ SCENARIO( "parse computations", "[HackCommandParser]" )
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D;JM" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D;JG" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D;" ) );
-
-    // valid destination, computation and jump
-    REQUIRE( Hasm::HackCommandParser::isComputationCommand( "D=D+1;JMP" ) );
 
     // invalid destination, valid computation and jump
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "d=D+1;JMP" ) );
@@ -52,14 +94,4 @@ SCENARIO( "parse computations", "[HackCommandParser]" )
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=A+1;jmp" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=D+1;JM" ) );
     REQUIRE_FALSE( Hasm::HackCommandParser::isComputationCommand( "D=M+1;" ) );
-}
-
-SCENARIO( "parse loads", "[HackCommandParser]" )
-{
-    REQUIRE( Hasm::HackCommandParser::isLoadCommand( "@10" ) );
-    REQUIRE( Hasm::HackCommandParser::isLoadCommand( "@symbol" ) );
-    REQUIRE_FALSE( Hasm::HackCommandParser::isLoadCommand( "@" ) );
-    REQUIRE_FALSE( Hasm::HackCommandParser::isLoadCommand( "@+1" ) );
-    REQUIRE_FALSE( Hasm::HackCommandParser::isLoadCommand( "10" ) );
-    REQUIRE_FALSE( Hasm::HackCommandParser::isLoadCommand( "id" ) );
 }
