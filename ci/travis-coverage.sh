bdd532cc7d203a1be0dfb96ff0adc0ffa4ef2567
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-26 18:15:34 +0200

    Force usr, tests and libs folder

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -4,7 +4,7 @@ if [ "$BUILD_TYPE" == "Coverage" ]; then
   cd ${TRAVIS_BUILD_DIR}
   tool="--gcov-tool $(which ${GCOV})"
   lcov ${tool} -d . -c -o coverage.info
-  lcov ${tool} -r coverage.info '/usr/*' 'tests/*' 'libs/*' -o coverage.info
+  #lcov ${tool} -r coverage.info '/usr/*' 'tests/*' 'libs/*' -o coverage.info
   lcov ${tool} -l coverage.info
   bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
 fi
