6098b1ff54e35eefaf27100d77912ec5272e42e7
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-26 13:07:35 +0200

    Point coverage to the correct root folder

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-pwd
-ls
+set -x
 
 if [ "$BUILD_TYPE" == "Coverage" ]; then
+  cd ${TRAVIS_BUILD_DIR}
+  pwd
+  ls
   gcovtool="--gcov $(which ${GCOV})"
   excludes="--exclude tests --exclude libs"
-  rootdirs="--root .. --build-root ."
+  rootdirs="--root . --build-root build"
   gcovopts="--gcov-options '\-lp'"
   coveralls ${gcovtool} ${excludes} ${rootdirs} ${gcovopts}
 fi
