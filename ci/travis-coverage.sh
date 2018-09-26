01dc93a9f220a2b896abe7d544249f3b2f43462e
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-26 17:49:05 +0200

    Remove gcov auto run

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -6,5 +6,5 @@ if [ "$BUILD_TYPE" == "Coverage" ]; then
   lcov ${tool} -d . -c -o coverage.info
   lcov ${tool} -r coverage.info '/usr/*' 'tests/*' 'libs/*' -o coverage.info
   lcov ${tool} -l coverage.info
-  bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+  bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
 fi
