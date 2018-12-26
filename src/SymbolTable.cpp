825df6f52328c7b1c0c31711775af6e795ff4002
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-26 11:07:59 +0100

    Debugging windows build

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,6 +1,6 @@
 #include "SymbolTable.hpp"
 
-#include <boost/range/adaptors.hpp>
+//#include <boost/range/adaptors.hpp>
 
 namespace Hasm
 {
@@ -29,7 +29,17 @@ boost::optional<Hack::word> SymbolTable::getAddress( const std::string& symbol )
 
 std::vector<std::string> SymbolTable::getSymbols() const
 {
-    return boost::copy_range<std::vector<std::string>>( boost::adaptors::keys( m_table ) );
+    std::vector<std::string> symbols;
+    symbols.reserve( m_table.size() );
+
+    for ( const auto& it : m_table )
+    {
+        symbols.emplace_back( it.first );
+    }
+
+    return symbols;
+
+    //return boost::copy_range<std::vector<std::string>>( boost::adaptors::keys( m_table ) );
 }
 
 } // namespace Hasm
