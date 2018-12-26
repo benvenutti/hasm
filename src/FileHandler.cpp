e2da0e0c722c570f54d4b355b6f14d33efa7b03a
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-26 11:31:31 +0100

    Small cleanup on Filehandler

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -8,27 +8,17 @@ namespace Hasm
 
 std::string FileHandler::changeExtension( const std::string& file, const std::string& newExtension )
 {
-    boost::filesystem::path path{ file };
-    path.replace_extension( newExtension );
-    const auto pathString = path.string();
-
-    return pathString;
+    return boost::filesystem::path{ file }.replace_extension( newExtension ).string();
 }
 
 bool FileHandler::hasExtension( const std::string& file, const std::string& extension )
 {
-    const boost::filesystem::path path{ file };
-    const auto                    isEqual = boost::iequals( path.extension().string(), extension );
-
-    return isEqual;
+    return boost::iequals( boost::filesystem::path{ file }.extension().string(), extension );
 }
 
 bool FileHandler::isFile( const std::string& file )
 {
-    const boost::filesystem::path path{ file };
-    const auto                    isRegularFile = boost::filesystem::is_regular_file( path );
-
-    return isRegularFile;
+    return boost::filesystem::is_regular_file( boost::filesystem::path{ file } );
 }
 
 } // namespace Hasm
