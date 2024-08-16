07a63272bd0f39b3d6b070d535f6e942af6cffa5
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2024-08-16 14:37:59 +0200

    Small cleanup in HackCommandParser
    Using nested namespaces and reducing the scope of the regex local variables.

--- src/HackCommandParser.cpp
+++ src/HackCommandParser.cpp
@@ -7,28 +7,25 @@
 namespace Hasm
 {
 
-namespace
-{
-
-const std::regex labelCmdRegex{ Hack::Grammar::LABEL };
-const std::regex compCmdRegex{ Hack::Grammar::COMPUTATION_COMMAND };
-const std::regex loadCmdRegex{ Hack::Grammar::LOAD };
-
-} // namespace
-
 bool HackCommandParser::isComputationCommand( const std::string& cmd )
 {
-    return std::regex_match( cmd, compCmdRegex );
+    static const std::regex regex{ Hack::Grammar::COMPUTATION_COMMAND };
+
+    return std::regex_match( cmd, regex );
 }
 
 bool HackCommandParser::isLabelCommand( const std::string& cmd )
 {
-    return std::regex_match( cmd, labelCmdRegex );
+    static const std::regex regex{ Hack::Grammar::LABEL };
+
+    return std::regex_match( cmd, regex );
 }
 
 bool HackCommandParser::isLoadCommand( const std::string& cmd )
 {
-    return std::regex_match( cmd, loadCmdRegex );
+    static const std::regex regex{ Hack::Grammar::LOAD };
+
+    return std::regex_match( cmd, regex );
 }
 
 } // namespace Hasm
