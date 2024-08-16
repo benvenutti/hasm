cecd511fb1ecf7c0e55511582138e056e58110e9
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2024-08-16 11:20:19 +0200

    Use std filesystem instead of boost filesystem

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -1,14 +1,15 @@
 #include "FileHandler.hpp"
 
 #include <boost/algorithm/string.hpp>
-#include <boost/filesystem.hpp>
+
+#include <filesystem>
 
 namespace Hasm
 {
 
 std::string FileHandler::changeExtension( const std::string& file, const std::string& newExtension )
 {
-    boost::filesystem::path path{ file };
+    std::filesystem::path path{ file };
     path.replace_extension( newExtension );
     const auto pathString = path.string();
 
@@ -17,7 +18,7 @@ std::string FileHandler::changeExtension( const std::string& file, const std::st
 
 bool FileHandler::hasExtension( const std::string& file, const std::string& extension )
 {
-    const boost::filesystem::path path{ file };
+    const std::filesystem::path   path{ file };
     const auto                    isEqual = boost::iequals( path.extension().string(), extension );
 
     return isEqual;
@@ -25,8 +26,8 @@ bool FileHandler::hasExtension( const std::string& file, const std::string& exte
 
 bool FileHandler::isFile( const std::string& file )
 {
-    const boost::filesystem::path path{ file };
-    const auto                    isRegularFile = boost::filesystem::is_regular_file( path );
+    const std::filesystem::path path{ file };
+    const auto                  isRegularFile = std::filesystem::is_regular_file( path );
 
     return isRegularFile;
 }
