a9adf6106d569d5363a6cca7573a1af7f93cb6a2
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2024-08-23 15:30:27 +0200

    Use std format to write symbol tables to the output stream

--- hasm/src/hasm/SymbolTableWriter.cpp
+++ hasm/src/hasm/SymbolTableWriter.cpp
@@ -2,7 +2,7 @@
 
 #include <hasm/SymbolTable.hpp>
 
-#include <iomanip>
+#include <format>
 
 namespace Hasm
 {
@@ -17,15 +17,12 @@ SymbolTableWriter::SymbolTableWriter( const SymbolTable& symbolTable )
 
 void SymbolTableWriter::write( std::ostream& out )
 {
-    const std::ios_base::fmtflags flags( out.flags() );
-
     for ( const auto& [address, symbol] : m_symbolMap )
     {
-        out << "0x" << std::setfill( '0' ) << std::setw( 4 ) << std::hex << address << " " << symbol << '\n';
+        out << std::format( "{:#06x} {}\n", address, symbol );
     }
 
     out.flush();
-    out.flags( flags );
 }
 
 } // namespace Hasm
