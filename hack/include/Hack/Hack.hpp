f9323cd91a82e28cb131766d5b1e2a6862a4bfa8
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2024-09-14 14:01:11 +0200

    Use constexpr for Hack constants

--- hack/include/Hack/Hack.hpp
+++ hack/include/Hack/Hack.hpp
@@ -7,9 +7,9 @@
 namespace Hack
 {
 
-const Hack::word init_ram_address{ 16_w };
+constexpr Hack::word init_ram_address{ 16_w };
 
 // max value for unsigned 15 bit number
-const Hack::word max_loadable_value = std::numeric_limits< Hack::word >::max() >> 1;
+constexpr Hack::word max_loadable_value = std::numeric_limits< Hack::word >::max() >> 1;
 
 } // namespace Hack
