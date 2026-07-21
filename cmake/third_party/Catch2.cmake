55171920e296d60a90f4c3a6888614366f5e5752
benvenutti <diogo.a.benvenutti@gmail.com>
2026-07-21 20:12:57 +0200

    chore: add cmake module to fetch Catch2

---
+++ cmake/third_party/Catch2.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    Catch2
+    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+    GIT_TAG        v3.7.0
+)
+
+FetchContent_MakeAvailable(Catch2)
